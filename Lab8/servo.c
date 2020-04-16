9528c5eef967a5520e5bba0ef6f393cb060ff0ca
sethbrad <sethbrad@iastate.edu>
2020-04-16 00:02:30 -0500

    final edit before test submission

--- Lab8/servo.c
+++ Lab8/servo.c
@@ -36,11 +36,14 @@ int servo_move(float degrees)
     int low_time = 0.02 - high_time;
     int match_val = low_time * 16000000;
 
+    int upper_part = match_val << 16; //grab the first two nibbles
+    int lower_part = match_val & 0x00FFFF; //mask off the first two nibbles
+
     TIMER1_TBPMR_R &= ~0xFF;
-    //TIMER1_TBPMR_R //Set prescale match val
+    TIMER1_TBPMR_R |= upper_part; //Set prescale match val
 
     TIMER1_TBMATCHR_R &= ~0xFFFF;
-    //TIMER1_TBMATCHR_R //Set match val
+    TIMER1_TBMATCHR_R |= lower_part; //Set match val
 
     TIMER1_CTL_R |= 0x100; //Start the timer
 
